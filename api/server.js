615e5947c56b3d6c7c03559bdaff8452c3f38615
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-01-18 12:05:39 -0800

    post to /api/dog in thenified and try/catch

--- api/server.js
+++ api/server.js
@@ -1,1 +1,42 @@
 // write the server here and export it
+
+const Dog = require("./dog-model");
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      "was able to get the server up and running by myself, with gitignore and nodemon running 💅 "
+    );
+});
+
+// thenified Dog
+server.post("/api/dogs", (req, res) => {
+  const dog = req.body;
+
+  Dog.create(dog)
+    .then((newlyCreatedDog) => {
+      res.status(200).json(newlyCreatedDog);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// try/catch Dog
+// server.post("/api/dogs", async (req, res) => {
+//   const dog = req.body;
+//   try {
+//     const newlyCreatedDog = await Dog.create(dog);
+//     res.status(200).json(newlyCreatedDog);
+//   } catch (err) {
+//     res.status(500).json({ error: err.message });
+//   }
+// });
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // import the server and start it
+
+const server = require("./api/server");
+
+const PORT = 8000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
